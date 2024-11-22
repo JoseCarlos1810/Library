e8aa7e75a114124a102b8973e0412850b01fc05c
JoseCarlos1810 <A01708776@tec.mx>
2024-11-21 23:31:07 -0600

    Project delivery 3

--- item.cpp
+++ item.cpp
@@ -2,20 +2,34 @@
 #include <iostream>
 using namespace std;
 
-Item::Item(string _title,string _hallway){
+//default constructur
+Item::Item(){
+    
+}
+//here the methods begin
+void Item::setTitle(string _title){
+    //setter
     title=_title;
+}
+
+void Item::setHallway(string _hallway){
+    //setter
     hallway=_hallway;
 }
 
-void Item::setTitle(string){
-    
-}
-
-void Item::setHallway(string){
-    
+void Item::setAvailable(bool _available){
+    //setter
+    available=_available;
+    if (available==true)
+        available2="true";
+    else
+        available2="false";
 }
 
 string Item::getItemData(){
-    return title,hallway;
+    //getter
+    string mensaje="The title of the item is: "+title+", it is in the hallway: "+hallway+", the item is available: "+available2;
+    return mensaje;
 }
+
 
