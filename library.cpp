7b616cd3eff75ffc4ab0783a52b551c41b5bfcc7
JoseCarlos1810 <A01708776@tec.mx>
2024-11-21 23:33:20 -0600

    Project delivery 3

--- library.cpp
+++ library.cpp
@@ -1,15 +1,29 @@
 #include "library.h"
 #include <iostream>
 using namespace std;
-
-Library::Library(bool _available){
-    available=_available;
+//default constructur with conposition
+Library::Library(){
+    book1.setTitle("Odisea");
+    book1.setHallway("C1");
+    book1.setGender("Odisea");
+    book1.setAuthor("Homero");
+    book1.setAvailable(true);
+    magazine1.setTitle("Expansion");
+    magazine1.setHallway("Q1");
+    magazine1.setType("Cientifica");
+    magazine1.setVolume(10);
+    magazine1.setAvailable(true);
+    user1.setBorrow(false);
+    user1.setReturns(true);
+    user1.setUserId("A01708776");
 }
-
-void Library::setAvailable(bool){
-
-}
-
-bool Library::getAvailable(){
-    return available;
+//here the methods begin
+string Library::getInfo(string option){
+    //getter
+    if (option=="book")
+        return book1.getBookData();
+    else if (option=="magazine")
+        return magazine1.getMagazineData();
+    else
+        return user1.getUserInfo();
 }
