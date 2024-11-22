1c42275059134051b2dac958d4875b8b67e5bd13
JoseCarlos1810 <A01708776@tec.mx>
2024-11-21 23:32:06 -0600

    Project delivery 3

--- item.h
+++ item.h
@@ -6,13 +6,17 @@ using namespace std;
 
 class Item{
     protected:
+        bool available;
         string title;
         string hallway;
+        string available2;
     public:
-        Item(string,string);
+        Item();
         void setTitle(string);
         void setHallway(string);
         string getItemData();
+        void setAvailable(bool);
+        string getAvailable();
 };
 
 
