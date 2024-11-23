c4245be749d655be76e8cf75ddac175351b40a5e
JoseCarlos1810 <A01708776@tec.mx>
2024-11-22 19:58:38 -0600

    Final delivery

--- magazine.h
+++ magazine.h
@@ -3,13 +3,14 @@
 #include "item.h"
 #include <iostream>
 using namespace std;
-
+//class of the magazines of the library, it has inheritance from item
 class Magazine:public Item{
     private:
         string type;
         int volume;
     public:
         Magazine();
+        Magazine( bool, string, string, string, int);
         void setType(string);
         void setVolume(int);
         string getMagazineData();
