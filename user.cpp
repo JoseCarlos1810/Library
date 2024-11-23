44f2cbd8f85d8ec521f916f71385d5ce43b0e0c0
JoseCarlos1810 <A01708776@tec.mx>
2024-11-22 19:54:36 -0600

    Final delivery

--- user.cpp
+++ user.cpp
@@ -1,28 +1,32 @@
 #include "user.h"
 #include <iostream>
 using namespace std;
-//default constructur
-User::User(){
-
+//default constructor
+User::User(){}
+//constructor
+User::User(string _userId, bool _borrow, bool _returns){
+    userId=_userId;
+    borrow=_borrow;
+    returns=_returns;
 }
 //here the methods begin
 void User::setUserId(string _userId){
-    //setter
+    //This is a setter of the Id of the user
     userId=_userId;
 }
 
 void User::setBorrow(bool _borrow){
-    //setter
+    //This is a setter to see if the user borrowed something
     borrow=_borrow;
 }
 
 void User::setReturns(bool _returns){
-    //setter
+    //This is a setter to see if the user returned what he borrowed
     returns=_returns;
 }
 
-string User::getUserInfo(){
-    //getter
+string User::getUserData(){
+    //This is a getter that return a mesage with all the information of a user
     string mensaje="The user id is: "+userId;
     return mensaje;
 }
