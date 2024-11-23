48f8566c2395e274979a5ce63b9bc1a59b03510a
JoseCarlos1810 <A01708776@tec.mx>
2024-11-22 19:55:32 -0600

    Final delivery

--- Book.cpp
+++ Book.cpp
@@ -2,24 +2,31 @@
 #include <iostream>
 using namespace std;
 
-//default constructur with inheritance
-Book::Book():Item(){
-    
+//default constructor with inheritance
+Book::Book():Item(){}
+
+//constructor with inheritance
+Book::Book(bool _available, string _title, string _hallway, string _author, string _gender):Item(_available, _title, _hallway){
+    author=_author;
+    gender=_gender;
 }
+
+
+
 //here the methods begin
 void Book::setAuthor(string _author){
-    //setter
+    //This is a setter of the author of the book
     author=_author;
 }
 
 void Book::setGender(string _gender){
-    //setter
+    //This is a setter of the gender of the book
     gender=_gender;
 }
 
 string Book::getBookData(){
-    //getter
-    string mensaje="The title of the book is: "+title+", it is in the hallway: "+hallway+", the author of the book is:"
-    +author+", the gender of the book is:"+gender+", the book is available: "+available2;
+    //This is a getter that return a mesage with all the information of a book
+    string mensaje="The title of the book is: "+title+", it is in the hallway: "+hallway+", \nthe author of the book is: "
+    +author+", the gender of the book is: "+gender+", \nthe book is available: "+available2;
     return mensaje;
 }
