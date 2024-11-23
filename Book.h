47984b23d429932d2271afc3240958f472b76bb5
JoseCarlos1810 <A01708776@tec.mx>
2024-11-22 19:55:55 -0600

    Final delivery

--- Book.h
+++ Book.h
@@ -4,13 +4,14 @@
 #include "item.h"
 #include <iostream>
 using namespace std;
-
+//class of the books of the library, it has inheritance from item
 class Book:public Item{
     private:
         string author;
         string gender;
     public:
         Book();
+        Book(bool, string, string, string, string);
         void setAuthor(string);
         void setGender(string);
         string getBookData();
