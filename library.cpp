92dea176f1fceb7fcc064793db412eef7c9e12d2
JoseCarlos1810 <A01708776@tec.mx>
2024-11-22 19:57:28 -0600

    Final delivery

--- library.cpp
+++ library.cpp
@@ -1,29 +1,49 @@
 #include "library.h"
 #include <iostream>
 using namespace std;
-//default constructur with conposition
+//default constructor with conposition
 Library::Library(){
-    book1.setTitle("Odisea");
-    book1.setHallway("C1");
-    book1.setGender("Odisea");
-    book1.setAuthor("Homero");
-    book1.setAvailable(true);
-    magazine1.setTitle("Expansion");
-    magazine1.setHallway("Q1");
-    magazine1.setType("Cientifica");
-    magazine1.setVolume(10);
-    magazine1.setAvailable(true);
-    user1.setBorrow(false);
-    user1.setReturns(true);
-    user1.setUserId("A01708776");
+
 }
 //here the methods begin
-string Library::getInfo(string option){
-    //getter
-    if (option=="book")
-        return book1.getBookData();
-    else if (option=="magazine")
-        return magazine1.getMagazineData();
-    else
-        return user1.getUserInfo();
+void Library::addBook(Book book, int lenght){
+    //this will add a book to the array of the books
+    books[lenght]=book;
+}
+
+void Library::addMagazine(Magazine magazine, int lenght){
+    //this will add a book to the array of the magazine
+    magazines[lenght]=magazine;
+}
+
+void Library::addUser(User user, int lenght){
+    //this will add a book to the array of the users
+    users[lenght]=user;
+}
+
+string Library::getBooksInfo(int cont){
+    //this will get all the info of the books in the array and fuse all the info in one message
+    stringstream aux;
+    for(int i = 0 ; i <cont; i++){
+        aux <<"Book " << i <<": "<< books[i].getBookData()<<"\n "<<endl;
+    }
+    return aux.str();
+}
+
+string Library::getMagazinesInfo(int cont){
+    //this will get all the info of the magazines in the array and fuse all the info in one message
+    stringstream aux;
+    for(int i = 0 ; i <cont; i++){
+        aux <<"Magazine " << i<<": " << magazines[i].getMagazineData()<<"\n "<< endl;
+    }
+    return aux.str();
+}
+
+string Library::getUsersInfo(int cont){
+    //this will get all the info of the users in the array and fuse all the info in one message
+    stringstream aux;
+    for(int i = 0 ; i <cont; i++){
+        aux <<"User " << i <<": "<< users[i].getUserData()<<"\n "<< endl;
+    }
+    return aux.str();
 }
