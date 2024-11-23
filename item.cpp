8d29a244819ca91b14688b1b341a1abd1b1f0198
JoseCarlos1810 <A01708776@tec.mx>
2024-11-22 19:56:21 -0600

    Final delivery

--- item.cpp
+++ item.cpp
@@ -2,34 +2,46 @@
 #include <iostream>
 using namespace std;
 
-//default constructur
-Item::Item(){
-    
+//default constructor
+Item::Item(){}
+//constructor
+Item::Item(bool _available, string _title,string _hallway){
+    available=_available;
+    title=_title;
+    hallway=_hallway;
+    if (available==true)
+        available2="true";
+    else
+        available2="false";
 }
 //here the methods begin
 void Item::setTitle(string _title){
-    //setter
+    //This is a setter of the title 
     title=_title;
 }
 
 void Item::setHallway(string _hallway){
-    //setter
+    //This is a setter of the hallway
     hallway=_hallway;
 }
 
 void Item::setAvailable(bool _available){
-    //setter
+    //This is a setter to see if the item is available
     available=_available;
     if (available==true)
         available2="true";
     else
         available2="false";
 }
-
+string Item::getAvailable(){
+    //This is a getter of available
+    return available2;
+}
 string Item::getItemData(){
-    //getter
+    //This is a getter that return a mesage with all the information of an item
     string mensaje="The title of the item is: "+title+", it is in the hallway: "+hallway+", the item is available: "+available2;
     return mensaje;
 }
+
 
 
